ba9e3d0e0c95c0371eb5fd6a640145623407a4a0
tony <tuyuan2012@163.com>
2014-01-13 13:02:34 +0800

    立项测试

---
+++ testMavenMybati/src/main/java/org/cocool/model/User.java
@@ -0,0 +1,75 @@
+package org.cocool.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private Date createdatetime;
+
+    private Date modifydatetime;
+
+    private String name;
+
+    private String pwd;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreatedatetime() {
+        return createdatetime;
+    }
+
+    public void setCreatedatetime(Date createdatetime) {
+        this.createdatetime = createdatetime;
+    }
+
+    public Date getModifydatetime() {
+        return modifydatetime;
+    }
+
+    public void setModifydatetime(Date modifydatetime) {
+        this.modifydatetime = modifydatetime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd == null ? null : pwd.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
